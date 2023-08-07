dcd37f5535c249df16acb1eb358478c56f7b2499
yashgupta31 <yashsntoshgupta2019@gmail.com>
2023-08-07 18:10:59 +0530

    first commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import "./Card.css"
 
 const Card = ({userName, profilePic, profileUrl}) => {
+    let follower = Math.floor(Math.random() * 900);
+    let following = Math.floor(Math.random() * 90);
     return (
         <>
             <div className="card">
                 <img src= {profilePic} alt="userImage" className="user-image" />
                 <h3 id="user-name">{userName}</h3>
-                <a href={profileUrl} className="btn" target="_blank">
+                <div className="detail-container">
+                    <div className="Follower">
+                        <spam>Followers</spam>
+                        <spam>{follower}k</spam>
+                    </div>
+
+                    <div className="Following">
+                        <spam>Following</spam>
+                        <spam>{following}</spam>
+                    </div>
+                </div>
+                <a href={profileUrl} className="btn" target="_blank"  rel="noreferrer">
                     Visit Profile</a>
             </div>
         </>
